35365fffe38ccd829239a513c9d9d0dab68a4a82
Jackie Chung <jackie.chung@utp.ac.pa>
2026-06-08 15:53:31 -0500

    La aplicacion corre, pero puede que hay que cambiar todo

--- app/src/main/java/com/utp/avance2_proyectofinal/Activities/MainActivity.kt
+++ app/src/main/java/com/utp/avance2_proyectofinal/Activities/MainActivity.kt
@@ -1,16 +1,17 @@
-2package com.utp.avance2_proyectofinal
+package com.utp.avance2_proyectofinal.Activities
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.utp.avance2_proyectofinal.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.pantalla_principal)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
